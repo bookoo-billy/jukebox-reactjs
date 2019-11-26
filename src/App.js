79e597c533c9c7c916fca46950209fb4d03be5d4
Bookoo Billy <bookoo.billy@gmail.com>
2019-11-26 02:30:51 +0000

    Added AppBar. Limited size of songs result table width

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
 import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
 import React from 'react';
@@ -35,43 +43,56 @@ class App extends React.Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App">
-          <Search handleOnChange={this.handleOnChange} />
-          <Query query={gql`
-          query Songs($search: String) {
-            searchSongs(search: $search) {
-              id
-              name
-              album {
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">
+              Jukebox
+          </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="xl">
+          <Box my={4}>
+            <Search handleOnChange={this.handleOnChange} />
+            <Query query={gql`
+            query Songs($search: String) {
+              searchSongs(search: $search) {
                 id
                 name
-              }
-              artist {
-                id
-                name
+                album {
+                  id
+                  name
+                }
+                artist {
+                  id
+                  name
+                }
               }
             }
-          }
-        `}
-        variables={{search: this.state.search.value}}
-          >
-            {({ loading, error, data }) => {
-              if (loading) return <p>Loading...</p>;
-              if (error) {
-                console.log(error);
-                return <p>Error :(</p>;
-              }
+          `}
+              variables={{ search: this.state.search.value }}
+            >
+              {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>;
+                if (error) {
+                  console.log(error);
+                  return <p>Error :(</p>;
+                }
 
-              return (
-                <div>
-                  <Artist />
-                  <Album />
-                  <Songs songs={data.searchSongs} />
-                </div>
-              );
-            }}
-          </Query>
-        </div>
+                return (
+                  <div>
+                    <Artist />
+                    <Album />
+                    <Songs songs={data.searchSongs} />
+                  </div>
+                );
+              }}
+            </Query>
+          </Box>
+        </Container>
       </ApolloProvider>
     );
   }
