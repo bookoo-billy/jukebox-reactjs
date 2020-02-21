3efe6725bf568b8ede89a647d325b19858ef49d2
Bookoo Billy <bookoo.billy@gmail.com>
2020-02-21 01:52:37 +0000

    Fixed img alt property

--- src/Home.js
+++ src/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
         <img style={{display: "block"}} src="https://svgsilh.com/png/2882289-3f51b5.png" alt="play guitar instrument" />
         <a href="https://svgsilh.com/3f51b5/image/2882289.html">"play guitar instrument"</a> is licensed under <a href="https://creativecommons.org/licenses/cc0/1.0/?ref=ccsearch&atype=html" style={{marginRight: "5px"}}>CC0 1.0</a>
         <a href="https://creativecommons.org/licenses/cc0/1.0/?ref=ccsearch&atype=html" target="_blank" rel="noopener noreferrer" style={{display: "inline-block", whiteSpace: "none", marginTop: "2px", marginLeft: "3px", height: "22px !important"}}>
-            <img style={{height: "inherit", marginRight: "3px", display: "inline-block"}} src="https://search.creativecommons.org/static/img/cc_icon.svg" />
-            <img style={{height: "inherit", marginRight: "3px", display: "inline-block"}} src="https://search.creativecommons.org/static/img/cc-cc0_icon.svg" />
+            <img style={{height: "inherit", marginRight: "3px", display: "inline-block"}} src="https://search.creativecommons.org/static/img/cc_icon.svg" alt="creative commons logo" />
+            <img style={{height: "inherit", marginRight: "3px", display: "inline-block"}} src="https://search.creativecommons.org/static/img/cc-cc0_icon.svg" alt="createive commons logo zero" />
         </a>
     </p>
   );
